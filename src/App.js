5d3f69f7c34dc34fb2c7b90d1bf5f28ed220242b
djvon <vonengineers@gmail.com>
2023-06-06 19:34:28 -0500

    Use mui for styling

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+// mui
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+// components
+import Copyright from "./components/Copyright";
+// -----------------------------------------------------
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container component="main" sx={{ height: "100vh" }}>
+      <Typography component="h1" variant="h5" sx={{ color: "white" }}>
+        Coming Soon
+      </Typography>
+      <Copyright sx={{ mt: 8, mb: 4 }} />
+    </Container>
   );
 }
 
---
+++ src/components/Copyright.js
@@ -0,0 +1,15 @@
+// mui
+import Typography from "@mui/material/Typography";
+// -----------------------------------------------------
+
+function Copyright(props) {
+  return (
+    <Typography variant="body2" color="white" align="center" {...props}>
+      {"© "}
+      {new Date().getFullYear()}Revolving Store Inc. 293 Church StNew York, NY
+      10013
+    </Typography>
+  );
+}
+
+export default Copyright;
